fc6d1e700d8aa32e08af65c475d34240e28e7e08
土豆 <2179257617@qq.com>
2025-09-09 13:02:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ num=float(weight_str[:-2])
 unit=weight_str[:-2]
 if unit=="kg":
   pound=num*2.2046
-  print(f"对应的英制重量为{pound:.3f磅")
+  print(f"对应的英制重量为{pound:.3f}磅")
 elif unit=="pd":
   kg=num/2.2046
   print(f"对应的公制重量为{kg:.3f}公斤”)
